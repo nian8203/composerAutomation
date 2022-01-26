0acad1a3c3306c0979032c1fda2b0d79061a79ed
Nelson Rodriguez <Nelson.Rodriguez@bitsamericas.com>
2022-01-26 11:12:08 -0500

    implementacion de funciones

--- pruebaLoad.py
+++ pruebaLoad.py
@@ -9,19 +9,37 @@ with open("composer.json") as myJson:
 
 #print(datos["require"]['bits/oneapp_adminimal'])
 
-for d in datos["require"],:
-    #print(f"{d}\n")
-    x = d['bits/express_auth']
-    print(x)
-    if x != '1.0.7':
-        d.update({'bits/express_auth':'1.0.7'})
-    else:
-        print("es igual")
+
+def modificarDatos(datos):
+    for d in datos["require"],:
+        #print(f"{d}\n")
+        x = d['bits/express_auth']
+        print(x)
+        if x != '1.0.8':
+            d.update({'bits/express_auth':'1.0.8'})
+        else:
+            print("es igual")
+            
+        print(d['bits/express_auth'])
+        myJson.close()
+        
+
+    
+
+
+# for d in datos["require"],:
+#     #print(f"{d}\n")
+#     x = d['bits/express_auth']
+#     print(x)
+#     if x != '1.0.7':
+#         d.update({'bits/express_auth':'1.0.7'})
+#     else:
+#         print("es igual")
 
 
 
-print(d['bits/express_auth'])
-myJson.close()   
+# print(d['bits/express_auth'])
+# myJson.close()   
     
 print("\n===========================================================\n")
 
@@ -29,20 +47,25 @@ print("\n===========================================================\n")
 
 print("\n===========================================================\n")
 
-s = json.dumps(datos,indent=4) #convertirlo a texto oara guardar como json
-#print(s)
-f = open("composer.json","w")
-f.write(s)
-f.close()
+
+def generarJson(datos):
+    s = json.dumps(datos,indent=4) #convertirlo a texto oara guardar como json
+    print(s)
+    f = open("composer.json","w")
+    f.write(s)
+    f.close()
+    
+
+# s = json.dumps(datos,indent=4) #convertirlo a texto oara guardar como json
+# #print(s)
+# f = open("composer.json","w")
+# f.write(s)
+# f.close()
+
+modificarDatos(datos)
+generarJson(datos)
 
 print("\n===========================================================\n")
 copyfile("/home/nian/Documentos/composerAutomation/composer.json", "/home/nian/Documentos/composerAutomation/escritura2.txt")
 
-'''
-s = json.dumps(datos, indent=4)
-f = open ("composer.json","w")
-f.write(s)
-f.close()
-print(s)
-'''
 
